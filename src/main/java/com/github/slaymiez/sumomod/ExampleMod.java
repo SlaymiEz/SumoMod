9c5550d2f723e11de66d7d3960b204839efc3155
GitHub Action <action@github.com>
2024-03-03 17:45:54 +0000

    Initialize template repository proper
    Set package name to com.github.slaymiez.sumomod
    Set project name to SumoMod
    Set modid to sumomod

--- src/main/java/com/github/slaymiez/sumomod/ExampleMod.java
+++ src/main/java/com/github/slaymiez/sumomod/ExampleMod.java
@@ -1,10 +1,10 @@
-package com.example;
+package com.github.slaymiez.sumomod;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "sumomod", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
--- src/main/java/com/github/slaymiez/sumomod/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/slaymiez/sumomod/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.slaymiez.sumomod.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
